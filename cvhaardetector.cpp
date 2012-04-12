13f9359a8ede762b3ded2ee176ee51702cbc2826
Riccardo Ferrazzo <f.riccardo87@gmail.com>
2012-04-12 18:29:24 +0200

    it builds but haardetector isnt tested already

--- cvhaardetector.cpp
+++ cvhaardetector.cpp
@@ -5,5 +5,49 @@ CvHaarDetector::CvHaarDetector(QObject *parent) :
     QObject(parent)
 {
     storage = cvCreateMemStorage(0);
-    _classifier = cvLoadHaarClassifierCascade("haarcascades/haarcascade_frontalface_default.xml");
+    currentClassifier = QString("haarcascades/haarcascade_frontalface_default.xml");
+    _classifier = cvLoadHaarClassifierCascade("haarcascades/haarcascade_frontalface_default.xml", cvSize(0,0));
+    _scaleFactor = 1.1;
+    _minNeighbors = 3;
+    _method = HaarCannyPruning;
+}
+
+CvHaarDetector::~CvHaarDetector(){
+    cvReleaseMemStorage(&storage);
+    delete _classifier;
+}
+
+void CvHaarDetector::setClassifier(QString classifier){
+    currentClassifier = classifier;
+    _classifier = cvLoadHaarClassifierCascade(classifier.toAscii().data(), cvSize(0,0));
+}
+
+void CvHaarDetector::setScaleFactor(double scaleFactor){
+    _scaleFactor = scaleFactor;
+}
+
+void CvHaarDetector::setMinNeighbors(int minNeighbors){
+    _minNeighbors = minNeighbors;
+}
+
+void CvHaarDetector::setMethod(HaarMethod method){
+    _method = method;
+}
+
+void CvHaarDetector::haarDetect(QByteArray imageData, int imageWidth, int imageHeight){
+    IplImage *image = cvCreateImage(cvSize(imageWidth, imageHeight), IPL_DEPTH_8U, 3);
+    cvSetData(image, imageData.data(), imageWidth*3);
+    cvClearMemStorage(storage);
+    _results = cvHaarDetectObjects(image, _classifier, storage, _scaleFactor, _minNeighbors, _method);
+    emit resultsReady();
+    cvReleaseImage(&image);
+}
+
+QVariantList CvHaarDetector::results(){
+    QVariantList res;
+    for(int i=0; i<_results->total; i++){
+        CvRect *r = (CvRect*)cvGetSeqElem(_results, i);
+        res.append(QRect(r->x, r->y, r->width, r->height));
+    }
+    return res;
 }
--- cvhaardetector.h
+++ cvhaardetector.h
@@ -2,6 +2,8 @@
 #define CVHAARDETECTOR_H
 
 #include <QObject>
+#include <QVariantList>
+#include <QRect>
 #include <opencv/cv.h>
 
 class CvHaarDetector : public QObject
@@ -9,21 +11,34 @@ class CvHaarDetector : public QObject
     Q_OBJECT
     Q_DISABLE_COPY(CvHaarDetector)
     Q_ENUMS(HaarMethod)
-    Q_PROPERTY(QString classifier WRITE setClassifier)
+    Q_PROPERTY(QString classifier READ classifier WRITE setClassifier)
     Q_PROPERTY(double scaleFactor READ scaleFactor WRITE setScaleFactor)
     Q_PROPERTY(int minNeighbors READ minNeighbors WRITE setMinNeighbors)
     Q_PROPERTY(HaarMethod method READ method WRITE setMethod)
-    Q_PROPERTY(QVariant results READ results NOTIFY resultsReady)
+    Q_PROPERTY(QVariantList results READ results NOTIFY resultsReady)
 
 public:
     explicit CvHaarDetector(QObject *parent = 0);
+    ~CvHaarDetector();
     enum HaarMethod {HaarScaleImage = CV_HAAR_SCALE_IMAGE,
                      HaarRoughSearch = CV_HAAR_DO_ROUGH_SEARCH,
                      HaarCannyPruning = CV_HAAR_DO_CANNY_PRUNING,
                      HaarFeatureMax = CV_HAAR_FEATURE_MAX,
                      HaarBiggestObject = CV_HAAR_FIND_BIGGEST_OBJECT
                     };
-    
+    //SET methods
+    void setClassifier(QString classifier);
+    void setScaleFactor(double scaleFactor);
+    void setMinNeighbors(int minNeighbors);
+    void setMethod(HaarMethod method);
+
+    //GET methods
+    QString classifier() const {return currentClassifier;}
+    double scaleFactor() const {return _scaleFactor;}
+    int minNeighbors() const {return _minNeighbors;}
+    HaarMethod method() const {return _method;}
+    QVariantList results();
+
 signals:
     void resultsReady();
     
@@ -33,7 +48,8 @@ public slots:
 private:
     CvMemStorage *storage;
     CvSeq *_results;
-    CvHaarClassifier *_classifier;
+    CvHaarClassifierCascade *_classifier;
+    QString currentClassifier;
     double _scaleFactor;
     int _minNeighbors;
     HaarMethod _method;
